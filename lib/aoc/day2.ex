b50eb41eff011b2d5128eb565b62f6932e7ce4e7
Justin Morris <desk@pixelbloom.com>
2025-12-05 12:10:31 +1100

    Day 2 (Part 1)

---
+++ lib/aoc/day2.ex
@@ -0,0 +1,34 @@
+defmodule Aoc.Day2 do
+  require Integer
+
+  @doc """
+  https://adventofcode.com/2025/day/2
+
+  ## Example
+
+    iex> Aoc.Day2.part1(input: "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124")
+    1227775554
+
+  """
+  def part1(opts \\ []) do
+    opts
+    |> Keyword.get_lazy(:input, &input/0)
+    |> String.split([",", "-"], trim: true)
+    |> Stream.map(&String.to_integer/1)
+    |> Stream.chunk_every(2)
+    |> Stream.flat_map(&apply(Range, :new, &1))
+    |> Stream.map(&Integer.digits/1)
+    |> Stream.filter(&Integer.is_even(length(&1)))
+    |> Stream.map(&Enum.chunk_every(&1, round(length(&1) / 2)))
+    |> Enum.sum_by(fn
+      [a, b] when a == b -> Integer.undigits(a ++ b)
+      [_, _] -> 0
+    end)
+  end
+
+  defp input do
+    Application.app_dir(:aoc, "priv/inputs/day2")
+    |> File.read!()
+    |> String.trim()
+  end
+end
--- test/aoc_test.exs
+++ test/aoc_test.exs
@@ -1,4 +1,6 @@
 defmodule AocTest do
   use ExUnit.Case
   doctest Aoc
+  doctest Aoc.Day1
+  doctest Aoc.Day2
 end
