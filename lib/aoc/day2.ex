781d7160471fe9bdba59f2050c98ec72efe86d59
Justin Morris <desk@pixelbloom.com>
2025-12-16 11:25:53 +1100

    Riff on day 2 part 2 with `Task.async_stream`

--- lib/aoc/day2.ex
+++ lib/aoc/day2.ex
@@ -47,27 +47,30 @@ defmodule Aoc.Day2 do
     |> String.split([",", "-"], trim: true)
     |> Stream.map(&String.to_integer/1)
     |> Stream.chunk_every(2)
-    |> Stream.flat_map(&Range.to_list(apply(Range, :new, &1)))
-    |> Stream.map(&Integer.digits(&1))
-    |> Stream.flat_map(fn digits ->
-      Enum.reduce_while(Stream.with_index(digits, 1), [], fn {_, index}, invalid ->
-        case Enum.split(digits, index) do
-          {pattern, haystack} when length(pattern) > length(haystack) ->
-            {:halt, invalid}
+    |> Task.async_stream(fn [first, last] ->
+      Range.new(first, last)
+      |> Stream.map(&Integer.digits(&1))
+      |> Stream.flat_map(fn digits ->
+        Enum.reduce_while(Enum.with_index(digits, 1), [], fn {_, index}, invalid ->
+          case Enum.split(digits, index) do
+            {needle, haystack} when length(needle) > length(haystack) ->
+              {:halt, invalid}
 
-          {pattern, haystack} ->
-            haystack
-            |> Enum.chunk_every(length(pattern))
-            |> Enum.map(&(&1 == pattern))
-            |> Enum.all?()
-            |> case do
-              true -> {:halt, [Integer.undigits(digits) | invalid]}
-              false -> {:cont, invalid}
-            end
-        end
+            {needle, haystack} ->
+              haystack
+              |> Enum.chunk_every(length(needle))
+              |> Enum.map(fn x -> x == needle end)
+              |> Enum.all?()
+              |> case do
+                true -> {:halt, [Integer.undigits(digits) | invalid]}
+                false -> {:cont, invalid}
+              end
+          end
+        end)
       end)
+      |> Enum.sum()
     end)
-    |> Enum.sum()
+    |> Enum.sum_by(fn {:ok, sum} -> sum end)
   end
 
   defp input, do: String.trim(File.read!(Application.app_dir(:aoc, "priv/inputs/day2")))
